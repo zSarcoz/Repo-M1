cd8774ee7e3d02596c894360956426b0703b2878
zSarcoz <cesar.cottua@gmail.com>
2022-03-28 14:55:06 -0400

    Finish homework

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+ 
+  let bin = 0
+  let res;
+
+  let num_arr = num.split('').reverse()
+  for(let i = 0; i < num_arr.length; i++){
+    res = num_arr[i]*Math.pow(2,i)
+    bin += res
+  }
+  return bin;
+
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  let bin = [];
+
+  while (num > 0){
+    bin.unshift(num%2);
+    num = Math.floor(num/2);
+
+  }
+  return bin.join('');
 }
 
 
